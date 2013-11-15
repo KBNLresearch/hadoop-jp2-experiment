facf56d4624c240bf7f610799dbf39e73f9f350d
renevanderark <r.van.der.ark@gmail.com>
2013-11-15 08:23:15 +0100

    args

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-hadoop fs -rm -r OUTPUT
-hadoop jar target/jp2-workflow-1.0-SNAPSHOT.jar eu.scape_project.hadoop.ConversionRunner KEY OUTPUT /home/rar011/hadoop/tmp
-hadoop fs -cat OUTPUT/part-00000
+hadoop fs -rm -r $2
+hadoop jar target/jp2-workflow-1.0-SNAPSHOT.jar eu.scape_project.hadoop.ConversionRunner $1 $2 $3 
+hadoop fs -cat $2/part-00000 | less
