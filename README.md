68e0f9fd4eaef38145a79e48c0cfbd3ab97d6122
Rene van der Ark <r.van.der.ark@gmail.com>
2013-12-02 11:22:23 +0100

    dropped opj_compress test

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ Simple Hadoop conversion workflow using external tools
 ========================
 
 Use Hadoop map jobs to spawn single tiff to jp2 conversion- and validationworkflow by spawning external binaries on the local OS.
+This setup aims to emulate the actual workflow used for the TIFF to jp2 migration programme of the KB (national library of the Netherlands).
+
+
 
 Sadly some external binaries spawned in this setup are proprietary software:
 - kdu_expand
--- src/test/java/DependencyTest.java
+++ src/test/java/DependencyTest.java
@@ -15,22 +15,12 @@ public class DependencyTest {
 
     @Test
     public void testForKakadu() throws IOException {
-/*        String jp2 = ConversionRunner.class.getResource("/test.jp2").getFile();
+        String jp2 = ConversionRunner.class.getResource("/test.jp2").getFile();
         LocalFile tmptif = new LocalFile("tmp.tif");
         CliCommand cmd = new CliCommand(new LocalFile(jp2), tmptif);
         cmd.runCommand("kdu_expand", "-i", "#infile#", "-o", "#outfile#");
         assert(new File(tmptif.getAbsolutePath()).exists());
-        tmptif.delete();*/
-    }
-
-    @Test
-    public void testForOpjCompress() throws IOException {
-        String tif = ConversionRunner.class.getResource("/test.tif").getFile();
-        LocalFile tmpjp2 = new LocalFile("tmp.jp2");
-        new CliCommand(new LocalFile(tif), tmpjp2)
-                .runCommand("opj_compress", "-n", "6", "-t", "512,512", "-i", "#infile#", "-o", "#outfile#");
-        assert(new File(tmpjp2.getAbsolutePath()).exists());
-        tmpjp2.delete();
+        tmptif.delete();
     }
 
     @Test
